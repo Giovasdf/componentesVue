aaad85f33e363fe90ae6c552d5f67e3f76d4a13d
Giovanni Molina <giovanni.m.05@gmail.com>
2024-08-28 18:49:41 +0100

    🎉 init:
    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import HeaderView from './components/HeaderView.vue'
+import HeroView from './components/HeroView.vue'
+import PersonaView from './components/PersonaView.vue'
+export default {
+  data() {
+    return {
+      personas: [
+        { foto:"https://avatars.githubusercontent.com/u/499550?v=4",nombre:"Evan You",descripcion:"Creador de Vue Js"},
+        { foto:"https://cdn.britannica.com/99/124299-050-4B4D509F/Linus-Torvalds-2012.jpg?w=400&h=300&c=crop",nombre:"Linus Torvalds",descripcion:" Creador de Linux"}, 
+        { foto:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ3LCoanmhnVe5TieG8TSzsNgO1Zte4Z1Pqg&s",nombre:"Robert C. Martin",descripcion:" Creador de Clean Code"}
+        ,],
+    }
+  },
+  components: {
+    HeaderView,
+    HeroView,
+    PersonaView
+  }
+}
+</script>
+
+<template>
+  <HeaderView />
+  <!-- <HeroView/> -->
+  <section class="personas">
+    <div v-for="persona in personas" :key="persona">
+      <PersonaView :foto="persona.foto" 
+      :nombre="persona.nombre" 
+      :descripcion="persona.descripcion"/>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.personas {
+  width: 80%;
+  margin: 30px auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+}
+</style>
---
+++ src/components/HeroView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <img src="https://media.nomadicmatt.com/2024/ams_itinerary1.jpeg">
+    </div>
+</template>
